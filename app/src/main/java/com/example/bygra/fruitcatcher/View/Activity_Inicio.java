c4c4d2f1079d13570b60691032ceba2a7c253d3b
Miguel Jimenez Granizo <migueljg1997@gmail.com>
2018-12-18 16:08:08 +0100

    Start Layout

--- app/src/main/java/com/example/bygra/fruitcatcher/View/Activity_Inicio.java
+++ app/src/main/java/com/example/bygra/fruitcatcher/View/Activity_Inicio.java
@@ -2,6 +2,7 @@ package com.example.bygra.fruitcatcher.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bygra.fruitcatcher.R;
 
@@ -11,5 +12,27 @@ public class Activity_Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__inicio);
+        hideUI();
+    }
+
+    private void hideUI(){
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
+
+    public void setBackground(View v){
+        System.exit(0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideUI();
     }
 }
